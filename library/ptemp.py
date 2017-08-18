b837c3aefd218f3ac2e0c716ba30c408514d2d56
Daniel Cordero (bastion) <bastion@azolla.0xdc.host>
2017-08-18 14:34:00 +0000

    ptemp: rename variable

--- library/ptemp.py
+++ library/ptemp.py
@@ -6,16 +6,16 @@ try:
 except ImportError:
     import simplejson as json
 
-TEMPDIR="/sys/bus/iio/devices/iio:device0/"
+IIO="/sys/bus/iio/devices/iio:device0/"
 msg = {}
 
-with open(os.path.join(TEMPDIR, "in_temp0_raw")) as rawf:
+with open(os.path.join(IIO, "in_temp0_raw")) as rawf:
     msg["raw"] = float(rawf.read())
 
-with open(os.path.join(TEMPDIR, "in_temp0_offset")) as offsetf:
+with open(os.path.join(IIO, "in_temp0_offset")) as offsetf:
     msg["offset"] = float(offsetf.read())
 
-with open(os.path.join(TEMPDIR, "in_temp0_scale")) as scalef:
+with open(os.path.join(IIO, "in_temp0_scale")) as scalef:
     msg["scale"] = float(scalef.read())
 
 msg["result"]=((msg["scale"] / 1000.0) * (msg["raw"] + msg["offset"]))
